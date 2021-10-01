5836c9c6c4f2971cc155b4c707165550af81f2f8
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2021-10-01 20:26:22 +0200

    update path

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import discord
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('server/.env')
+dotenv_path = Path('./server/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
