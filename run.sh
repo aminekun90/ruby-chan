3e93023229cba7c07c34da2032bc0efc061434da
Amine Bouzahar <aminebouzahar@Amines-MacBook-Pro.local>
2022-05-22 17:11:53 +0200

    Add run.sh file and Mac os support, upgrade to python 3.10

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## Usage:
+##   . ./export-env.sh ; $COMMAND
+##   . ./export-env.sh ; echo ${MINIENTREGA_FECHALIMITE}
+envpath=server/.env
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ] || [ "$unamestr" = 'Darwin' ]; then
+
+  export $(grep -v '^#' $envpath | xargs -d '\n')
+
+elif [ "$unamestr" = 'FreeBSD' ]; then
+
+  export $(grep -v '^#' $envpath | xargs -0)
+
+fi
+
+source venv/bin/activate
+python3 bot.py
